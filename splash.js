f4ad2bd39491c0bda33ad687c7acd1552c9269b0
Noelle <53459848+noellenoe@users.noreply.github.com>
2023-03-28 08:56:21 -0400

    Update splash.js

--- splash.js
+++ splash.js
@@ -1,7 +1,12 @@
 const splashList = [
-"A",
-"B", 
-"C"
+"Anywhere can be a paradise as long as you have the will to live.",
+"Feel the vibes of this gal!",
+"What the tuna!",
+"What the scallop!",
+"First, the park, where we’ll see nice doggies.",
+"Then embark, and we’ll paint cute froggies.",
+"Does Bowser, Shinji, or Mr. Wolf own this site?",
+"Loss memes are not allowed."
 ] //write all your splashes here
 
 function loadSplash() {
