d230c4836fc19a97d86a95434a075ea927b42d33
Noelle <53459848+noellenoe@users.noreply.github.com>
2023-03-30 15:00:14 -0400

    Update splash.js

--- splash.js
+++ splash.js
@@ -1,13 +1,21 @@
 const splashList = [
 "Anywhere can be a paradise as long as you have the will to live.",
+"Listen to BIPOC voices",
 "Feel the vibes of this gal!",
 "What the tuna!",
+"Support your local LGBTQIA+ youth",
 "What the scallop!",
+"Support your local abortion clinics",
 "First, the park, where we’ll see nice doggies.",
+"Protest against bills that harm you and your friends",
 "Then embark, and we’ll paint cute froggies.",
+"You are loved",
 "Does Bowser, Shinji, or Mr. Wolf own this site?",
+"You are valid",
 "Loss memes are not allowed.",
+"You are beautiful",
 "Whats with these homies dissing my girl?",
+"You are amazing", 
 "Legalise poop"
 ] //write all your splashes here
 
